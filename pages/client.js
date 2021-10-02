41d04d3c7999db2fc0cbfdbdfacbd3173b1b51f3
Carlos Farro <fdcarlosd1@gmail.com>
2021-10-01 23:33:07 -0500

    change password to client

--- pages/client.js
+++ pages/client.js
@@ -6,6 +6,18 @@ import popupsContext from '../context/pop-ups/pop-upsContext';
 import { validateResArray } from '../config/helper';
 import IconButton from '@material-ui/core/IconButton';
 import authContext from '../context/auth/authContext';
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import InputFormk from '../components/system/form/inputformik';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import Button from '@material-ui/core/Button';
+import { useFormik } from 'formik';
+import * as Yup from 'yup';
+import {
+    Delete as DeleteIcon,
+    Edit as EditIcon,
+} from '@material-ui/icons';
 
 const selproperty = {
     method: "SP_SEL_CLIENT",
@@ -13,6 +25,125 @@ const selproperty = {
         name: null,
         status: 'ACTIVO'
     }
+}
+
+const UserModal = ({ title, openModal, setOpenModal, rowselected, fetchDataUser }) => {
+
+    const { setOpenBackdrop, setOpenSnackBack, setModalQuestion } = useContext(popupsContext);
+
+
+    const formik = useFormik({
+        enableReinitialize: true,
+        initialValues: rowselected || {
+            first_name: '',
+            last_name: '',
+            password: '',
+        },
+        validationSchema: Yup.object({
+            confirmpassword: Yup.string().oneOf([Yup.ref('password'), null], 'Passwords must match')
+        }),
+        onSubmit: async values => {
+            if (!values.password && !rowselected) {
+                setOpenSnackBack(true, { success: false, message: 'Debe ingresar una contraseña al usuario.' });
+                return;
+            }
+            // const thereisdefault = dataorg.some(x => x.bydefault === "SI");
+            // if (!thereisdefault) {
+            //     setOpenSnackBack(true, { success: false, message: 'Debe establecer una organización por defecto.' });
+            //     return;
+            // }
+            const callback = async () => {
+                setModalQuestion({ visible: false });
+                const dattosend = {
+                    method: "SP_INS_CLIENT",
+                    data: {
+                        id_user: rowselected?.id_user,
+                        username: rowselected?.username || '',
+                        first_name: rowselected?.first_name || '',
+                        last_name: rowselected?.last_name || '',
+                        doc_type: rowselected?.doc_type || '',
+                        doc_number: rowselected?.doc_number || '',
+                        user_email: rowselected?.user_email || '',
+                        phone: rowselected?.phone || '',
+                        status: rowselected?.status || '',
+                        type: rowselected?.type || 'CLIENTE',
+                        password: values.password ? values.password : "",
+                    }
+                }
+
+                setOpenBackdrop(true);
+                const res = await triggeraxios('post', process.env.endpoints.selsimple, dattosend);
+                if (res.success) {
+                    fetchDataUser({});
+                    setOpenModal(false);
+
+                    setOpenSnackBack(true, { success: true, message: 'Transacción ejecutada satisfactoriamente.' });
+                } else {
+                    setOpenSnackBack(true, { success: false, message: res.msg });
+                }
+
+                setOpenBackdrop(false);
+            }
+
+            setModalQuestion({ visible: true, question: `¿Está seguro de guardar el usuario?`, callback })
+        }
+    });
+
+    const handleClick = () => setOpenModal(false);
+
+    return (
+        <>
+            <Dialog
+                open={openModal}
+                fullWidth={true}
+                maxWidth='md'
+                onClose={handleClick}
+                aria-labelledby="alert-dialog-title"
+                aria-describedby="alert-dialog-description"
+            >
+                <form
+                    noValidate
+                    onSubmit={formik.handleSubmit}
+                >
+                    <DialogTitle id="alert-dialog-title">{title}</DialogTitle>
+                    <DialogContent>
+                        <div className="row-zyx">
+                            <InputFormk
+                                classname="col-6"
+                                name="password"
+                                label="Contraseña"
+                                type="password"
+                                formik={formik}
+                            />
+                            <InputFormk
+                                classname="col-6"
+                                name="confirmpassword"
+                                label="Confirmar Contraseña"
+                                type="password"
+                                formik={formik}
+                            />
+                        </div>
+                    </DialogContent>
+                    <DialogActions>
+                        <Button
+                            type="submit"
+                            color="primary"
+                        >
+                            GUARDAR
+                        </Button>
+                        <Button
+                            type="button"
+                            color="secondary"
+                            style={{ marginLeft: '1rem' }}
+                            onClick={() => setOpenModal(false)}
+                        >
+                            Cerrar
+                        </Button>
+                    </DialogActions>
+                </form>
+            </Dialog>
+        </>
+    );
 }
 
 const Client = () => {
@@ -24,7 +155,31 @@ const Client = () => {
 
     const columns = React.useMemo(
         () => [
-           
+            {
+                Header: '',
+                accessor: 'id_user',
+                activeOnHover: true,
+                Cell: props => {
+                    return (
+                        <div className="container-button-floating">
+                            <IconButton
+                                aria-label="delete"
+                                size="small"
+                                className="button-floating"
+                                disabled={!appfound.update}
+                                onClick={() => {
+                                    selectrow(props.cell.row.original);
+                                }}
+                            >
+                                <EditIcon
+                                    fontSize="inherit"
+                                    size="small"
+                                />
+                            </IconButton>
+                        </div>
+                    )
+                }
+            },
             {
                 Header: 'Nombre',
                 accessor: 'first_name',
@@ -54,6 +209,11 @@ const Client = () => {
         [appfound]
     );
 
+    const selectrow = (row) => {
+        setOpenModal(true);
+        setrowselected(row);
+    }
+
     const fetchData = React.useCallback(async () => {
         setloadingglobal(true);
 
@@ -64,32 +224,6 @@ const Client = () => {
         setloadingglobal(false)
     }, []);
 
-
-    const deleterow = (row) => {
-        const callback = async () => {
-            setModalQuestion({ visible: false });
-            const datatosend = {
-                method: "SP_INS_PROPERTIES",
-                data: {
-                    ...row,
-                    status: 'ELIMINADO'
-                }
-            }
-
-            setOpenBackdrop(true);
-            const res = await triggeraxios('post', process.env.endpoints.selsimple, datatosend);
-            if (res.success) {
-                setOpenSnackBack(true, { success: true, message: 'La propiedad se eliminó satisfactoriamente.' });
-                fetchData();
-            } else 
-                setOpenSnackBack(true, { success: false, message: 'Hubo un error, vuelva a intentarlo' });
-
-            setOpenBackdrop(false)
-        }
-
-        setModalQuestion({ visible: true, question: `¿Está seguro de eliminar la propiedad ${row.name}?`, callback })
-    }
-
     return (
         <Layout>
             <TableZyx
@@ -98,6 +232,13 @@ const Client = () => {
                 data={datatable}
                 fetchData={fetchData}
                 register={false}
+            />
+            <UserModal 
+                title="Cambiar contraseña"
+                openModal={openModal}
+                setOpenModal={setOpenModal}
+                rowselected={rowselected}
+                fetchDataUser={fetchData}
             />
         </Layout>
     );

43e2e580d64a951c049cf38239e2d619686671b8
Carlos Farro <carlos.farro@vcaperu.com>
2021-10-20 12:05:38 -0500

    changes

---
+++ pages/new_products.js
@@ -0,0 +1,17 @@
+import React, { useState, useContext } from 'react';
+import Layout from '../components/system/layout/layout'
+import Box from '@material-ui/core/Box';
+import Paper from '@material-ui/core/Paper';
+
+const Home = () => {
+    
+    return (
+        <Layout>
+            <Box component={Paper} px={2} pt={2} mx={2} mb={2} style={{ height: '80vh' }}>
+                <h2>Módulo en desarrollo</h2>
+            </Box>
+        </Layout>
+    );
+}
+
+export default Home;
